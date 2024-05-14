add8ed40cf34b7e04e06161e3c3f5f45baa34e98
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-14 15:41:07 +0700

    commit13

--- constructor3.cpp
+++ constructor3.cpp
@@ -14,11 +14,13 @@ public:
         nim = 0;
         nama = "";
     };
+
     Mahasiswa(int iNim)
     {
         nim = iNim;
     
     };
+    
     Mahasiswa(int iNim, string iNama)
     {
         nim = iNim;
@@ -31,3 +33,22 @@ public:
         cout << endl;
     }
 };
+
+
+
+
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Indra");
+    Mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
