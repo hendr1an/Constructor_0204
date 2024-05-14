6bc5be7c76832dfaa6d27c833df6cbf7bfba9d75
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-14 15:07:46 +0700

    commit10

--- constructor2.cpp
+++ constructor2.cpp
@@ -28,5 +28,17 @@ int mahasiswa::nim = 0;
 
 void mahasiswa::setID()
 {
-    
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Sri Dadi");
 }
