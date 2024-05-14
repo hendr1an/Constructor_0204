32e19705e5071d3e1fba518934dfa0791c7e0c10
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-14 15:29:09 +0700

    commit13

--- constructor3.cpp
+++ constructor3.cpp
@@ -17,6 +17,17 @@ public:
     Mahasiswa(int iNim)
     {
         nim = iNim;
-    }
+    
+    };
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
 
+    void cetak(){
+        cout << "NIM  = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    }
 };
